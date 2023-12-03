3d3b2196e2ba84ccc413d5e7a72d213fcc0944d9
faz1241543245398645 <111303147+faz1241543245398645@users.noreply.github.com>
2023-12-04 00:55:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,29 +5,72 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    # Display a form for text input with some basic styling using CSS
     return """
-        <form action="/analyze" method="get">
-            <input type="text" name="text">
-            <input type="submit" value="analyse">
-        </form>
+        <head>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    margin: 20px;
+                }
+                form {
+                    margin-bottom: 20px;
+                }
+                input[type="text"] {
+                    padding: 8px;
+                    width: 300px;
+                }
+                input[type="submit"] {
+                    padding: 8px 16px;
+                    background-color: #4CAF50;
+                    color: white;
+                    border: none;
+                    cursor: pointer;
+                }
+                .sentiment {
+                    font-size: 18px;
+                    margin-top: 20px;
+                }
+                .positive {
+                    color: #008000;
+                }
+                .negative {
+                    color: #FF0000;
+                }
+                .neutral {
+                    color: #0000FF;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>Sentiment Analysis</h1>
+            <form action="/analyze" method="get">
+                <input type="text" name="text" placeholder="Enter text...">
+                <input type="submit" value="Analyze">
+            </form>
+            <div class="sentiment" id="sentiment">
+                <!-- Sentiment result will appear here -->
+            </div>
+        </body>
     """
 
 @app.route("/analyze")
 def analyze():
     text = request.args.get("text", "")
+
     if text:
         blob = TextBlob(text)
         sentiment_score = blob.sentiment.polarity
+        if sentiment_score > 0:
+            sentiment =  "Positive"
+        elif sentiment_score < 0:
+            sentiment =   "Negative"
+        else:
+            sentiment =   "Neutral"
+
+    return f'<div class="sentiment {sentiment.lower()}">Sentiment: {sentiment}</div>'
 
         # Classify sentiment based on score
-        if sentiment_score > 0:
-            return "Positive"
-        elif sentiment_score < 0:
-            return "Negative"
-        else:
-            return "Neutral"
-    else:
-        return "Please enter some text to analyze."
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
